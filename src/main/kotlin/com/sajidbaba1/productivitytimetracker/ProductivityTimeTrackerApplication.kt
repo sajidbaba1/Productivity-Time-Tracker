ff637a199028b5b35f85b1264620ede943b83965
sajid <thsajid831@gmail.com>
2025-08-01 14:26:29 +0530

    initial commit

---
+++ src/main/kotlin/com/sajidbaba1/productivitytimetracker/ProductivityTimeTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.sajidbaba1.productivitytimetracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductivityTimeTrackerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductivityTimeTrackerApplication>(*args)
+}
